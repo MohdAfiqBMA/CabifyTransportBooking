27e33daf72332ddf95d0e85cfe9f0eb6c39617a0
Afiq <2100591C@student.tp.edu.sg>
2023-01-16 17:28:06 +0800

    created first name last name in database

--- CabifyTransportBooking/Server/Models/ApplicationUser.cs
+++ CabifyTransportBooking/Server/Models/ApplicationUser.cs
@@ -8,8 +8,7 @@ namespace CabifyTransportBooking.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }\
-        
+        public string FirstName { get; set; }
         public string LastName { get; set; }
     }
 }
