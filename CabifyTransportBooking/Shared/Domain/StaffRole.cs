57ffeb200daffbda65451f2bb6024fcf00d2e93b
Afiq <2100591C@student.tp.edu.sg>
2023-01-16 16:33:00 +0800

    Created DbContext

---
+++ CabifyTransportBooking/Shared/Domain/StaffRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabifyTransportBooking.Shared.Domain
+{
+    public class StaffRole:BaseDomainModel
+    {
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        public string RoleType { get; set; }
+    }
+}
