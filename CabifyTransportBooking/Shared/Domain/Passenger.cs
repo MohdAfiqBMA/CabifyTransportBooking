2907676375b9d8773c1f929095a28c66187f47eb
Ming Jie <2104393J@student.tp.edu.sg>
2023-01-17 16:31:41 +0800

    -added vehicle category, booking, passenger classes

---
+++ CabifyTransportBooking/Shared/Domain/Passenger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabifyTransportBooking.Shared.Domain
+{
+    public class Passenger : BaseDomainModel
+    {
+        public string PassengerGender { get; set; }
+        public string PassengerName { get; set; }
+        public string PassengerPhone { get; set; }
+        public string PassengerEmail { get; set; }
+        public string PassengerPassword { get; set; }
+        public int PassengerRating { get; set; }
+        public string  PassengerAddress { get; set; }
+
+    }
+}
