03ea2c63d2f7a81c69d3c76681ebee7ccdd1d11f
Ming Jie <2104393J@student.tp.edu.sg>
2023-01-26 18:17:12 +0800

    - controller for passenger - endpoints - Creating List Pages

---
+++ CabifyTransportBooking/Client/Static/Endpoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CabifyTransportBooking.Client.Static
+{
+    public class Endpoint
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string PassengerEndpoint = $"{Prefix}/passenger";
+        public static readonly string StaffEndpoint = $"{Prefix}/staff";
+        public static readonly string VehicleEndpoint = $"{Prefix}/vehicle";
+        public static readonly string BookingEndpoint = $"{Prefix}/booking";
+    }
+}
