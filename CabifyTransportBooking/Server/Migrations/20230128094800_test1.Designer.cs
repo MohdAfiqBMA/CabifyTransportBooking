2b91fd9e6fd01b5c12f73053d1268f5899b7c7f0
Ming Jie <2104393J@student.tp.edu.sg>
2023-01-28 18:14:25 +0800

    -formcomponent

--- CabifyTransportBooking/Server/Migrations/20230128094800_test1.Designer.cs
+++ CabifyTransportBooking/Server/Migrations/20230128094800_test1.Designer.cs
@@ -4,14 +4,16 @@ using CabifyTransportBooking.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CabifyTransportBooking.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230128094800_test1")]
+    partial class test1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
